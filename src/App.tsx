223b424469a79449008c4df4c950e2a4efc6e952
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-08 00:04:22 +0300

    feat: use store in component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './styles/App.css';
+import {userSlice} from "./store/reducers/UserSlice.ts";
+import {useAppDispatch, useAppSelector} from "./hooks/redux.ts";
 
 function App() {
-
+    const {count} = useAppSelector(state => state.userReducer)
+    const {increment} = userSlice.actions;
+    const dispatch = useAppDispatch();
     return (
         <div className="app">
-            hello
+            <h1>{count}</h1>
+            <button onClick={() => dispatch(increment())}>Increment</button>
         </div>
     )
 }
