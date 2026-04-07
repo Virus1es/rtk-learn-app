75501d96cf670b31eab036e7a63005bc876ad6d4
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-07 23:50:34 +0300

    create: create empty user slice

---
+++ src/store/reducers/UserSlice.ts
@@ -0,0 +1,25 @@
+import type {IUser} from "../../models/IUser.ts";
+import {createSlice} from "@reduxjs/toolkit";
+
+interface UserState {
+    users: IUser[];
+    isLoading: boolean;
+    error: string;
+}
+
+const initialState: UserState = {
+    users: [],
+    isLoading: false,
+    error: "",
+}
+
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+
+    }
+});
+
+export default userSlice.reducer;
