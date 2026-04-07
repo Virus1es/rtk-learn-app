bc9303100344f87c427a332641463ac600c85886
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-08 00:04:02 +0300

    feat: connect store to app

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import {Provider} from "react-redux";
+import {setupStore} from "./store/store.ts";
 
-createRoot(document.getElementById('root')!).render(<App />)
+const store = setupStore();
+
+createRoot(document.getElementById('root')!).render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
