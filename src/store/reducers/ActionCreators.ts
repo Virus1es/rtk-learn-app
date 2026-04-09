287ee0f5303af91a790e0551a8f2bc245473d404
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-09 22:46:59 +0300

    feat: finish action to fetch users

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -1,11 +1,14 @@
 import type {AppDispatch} from "../store.ts";
 import axios from "axios";
 import type {IUser} from "../../models/IUser.ts";
+import {userSlice} from "./UserSlice.ts";
 
 export const fetchUsers = () => async (dispatch: AppDispatch) => {
     try {
+        dispatch(userSlice.actions.usersFetching());
         const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+        dispatch(userSlice.actions.usersFetchingSuccess(response.data));
     } catch (e) {
-        console.error(e);
+        dispatch(userSlice.actions.usersFetchingError(e.message));
     }
 }
