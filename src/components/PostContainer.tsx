ca824cedde9425fb558576d548ff5011e55b51ef
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-09 23:53:57 +0300

    feat: install and create json-server

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -3,10 +3,12 @@ import {postAPI} from "../services/PostService.ts";
 import PostItem from "./PostItem.tsx";
 
 const PostContainer = () => {
-    const {data: posts} = postAPI.useFetchAllPostsQuery(5);
+    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(5);
 
     return (
         <div className="post__list">
+            {isLoading && <h1>Идёт загрузка</h1>}
+            {error && <h1 style={{color: 'red'}}>Произошла ошибка {error}</h1>}
             {posts && posts.map(post =>
                 <PostItem key={post.id} post={post}/>
             )}
