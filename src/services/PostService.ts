4172fd4aa85f8df646c8c4524b5860375a03ca83
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-09 23:35:40 +0300

    fix: fix model name

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -3,7 +3,9 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const postAPI = createApi({
     reducerPath: 'postAPI',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com'}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://jsonplaceholder.typicode.com'
+    }),
     endpoints: (build) => ({
         fetchAllPosts: build.query({
             query: () => ({
@@ -11,4 +13,4 @@ export const postAPI = createApi({
             })
         })
     })
-})
+});
