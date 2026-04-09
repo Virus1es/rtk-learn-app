46097993b957dfb737c53340b34edad2b67260e0
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-09 22:52:18 +0300

    feat: use created actions and reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import './styles/App.css';
 import {userSlice} from "./store/reducers/UserSlice.ts";
 import {useAppDispatch, useAppSelector} from "./hooks/redux.ts";
+import {useEffect} from "react";
+import {fetchUsers} from "./store/reducers/ActionCreators.ts";
 
 function App() {
-    const {count} = useAppSelector(state => state.userReducer)
-    const {increment} = userSlice.actions;
     const dispatch = useAppDispatch();
+    const {users, isLoading, error} = useAppSelector(state => state.userReducer);
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
+
+
     return (
         <div className="app">
-            <h1>{count}</h1>
-            <button onClick={() => dispatch(increment(10))}>Increment</button>
+            {isLoading && <h1>Идёт загрузка...</h1>}
+            {error && <h1 style={{color: 'red'}}>Ошибка: {error}</h1>}
+            {users.length > 0 && JSON.stringify(users, null, 2)}
         </div>
     )
 }
