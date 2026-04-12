946f49f73bcbb444a53afa21719841c2dcc2a73d
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-04-12 22:39:08 +0300

    feat: use endpoint for create post

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {postAPI} from "../services/PostService.ts";
 import PostItem from "./PostItem.tsx";
+import type {IPost} from "../models/IPost.ts";
 
 const PostContainer = () => {
-    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery('');
+    const [limit, setLimit] = useState(100);
+    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(limit);
+
+    const [createPost, {}] = postAPI.useCreatePostMutation();
+
+    const handleCreate = async () => {
+        const title = prompt();
+        await createPost({title, body: title} as IPost);
+    }
 
     return (
         <div className="post__list">
+            <button onClick={handleCreate}>Add new post</button>
             {isLoading && <h1>Идёт загрузка</h1>}
             {error && <h1 style={{color: 'red'}}>Произошла ошибка</h1>}
             {posts && posts.map(post =>
